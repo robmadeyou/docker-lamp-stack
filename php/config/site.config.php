3ecc9e4dc855d9ff5d56b6f104be51c687f3913e
robmadeyou <robmadeyou@gmail.com>
2015-10-27 09:57:25 +0000

    Adding php configs for app

---
+++ php/config/site.config.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Your\WebApp;
+
+use Rhubarb\Crown\Exceptions\Handlers\ExceptionHandler;
+use Rhubarb\Crown\Logging\Log;
+use Rhubarb\Crown\Logging\PhpLog;
+use Rhubarb\Stem\StemSettings;
+use Rhubarb\Crown\Context;
+
+$dbSettings = new StemSettings();
+$dbSettings->Host = "feedsdb";
+$dbSettings->Username = "root";
+$dbSettings->Password = "password";
+$dbSettings->Database = "feeds";
+
+// Add a PHP logger
+Log::attachLog( new PhpLog( Log::ERROR_LEVEL ) );
+// // Switch off exception trapping. You should have this on in the production environment.
+ExceptionHandler::disableExceptionTrapping();
+
+$context = new Context();
+$context->DeveloperMode = true;
