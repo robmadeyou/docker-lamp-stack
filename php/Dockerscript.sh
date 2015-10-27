c136a9accdeafdf6a5e30dee944b345b4b21692f
robmadeyou <robmadeyou@gmail.com>
2015-10-27 10:44:17 +0000

    chmoding app folder for rw permissions

--- php/Dockerscript.sh
+++ php/Dockerscript.sh
@@ -2,7 +2,9 @@
 
 LOCATION=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 
+chmod -R ugo+rw app;
+
 cp configs/site.config.php app/settings/
 
 docker build -t img-app .
-docker run -it --rm -v $LOCATION/app:/var/www/app -p 8080:80 --name app img-app
+docker run -d --rm -v $LOCATION/app:/var/www/app -p 8080:80 --name app img-app
