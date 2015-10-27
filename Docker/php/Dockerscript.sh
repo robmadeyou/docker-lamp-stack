1e41346c331f0df7694dd0fed3d5bff3ba24a25c
robmadeyou <robmadeyou@gmail.com>
2015-10-27 10:47:59 +0000

    Moving files around

--- Docker/php/Dockerscript.sh
+++ Docker/php/Dockerscript.sh
@@ -7,4 +7,4 @@ chmod -R ugo+rw app;
 cp configs/site.config.php app/settings/
 
 docker build -t img-app .
-docker run -d --rm -v $LOCATION/app:/var/www/app -p 8080:80 --name app img-app
+docker run -d -it -v $LOCATION/../../app:/var/www/app -p 8080:80 --name app img-app
