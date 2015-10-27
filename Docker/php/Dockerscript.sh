da47b9d590ac5d38941717ef9d9d05dcebd06d13
robmadeyou <robmadeyou@gmail.com>
2015-10-27 11:33:27 +0000

    Changing chmod location and location of site.config

--- Docker/php/Dockerscript.sh
+++ Docker/php/Dockerscript.sh
@@ -2,9 +2,9 @@
 
 LOCATION=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 
-chmod -R ugo+rw app;
+chmod -R ugo+rw ../../app;
 
-cp configs/site.config.php app/settings/
+cp configs/site.config.php ../../app/settings/
 
 docker build -t img-app .
 docker run -d -it -v $LOCATION/../../app:/var/www/app -p 8080:80 --name app img-app
